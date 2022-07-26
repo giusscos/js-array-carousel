401d25b3353ada70c9864fd8afa00e0eec766c0e
giusscos <giu.cosenza08@gmail.com>
2022-07-26 16:16:20 +0000

    Add CSS and Slider Img in  JS

---
+++ js/app.js
@@ -0,0 +1,32 @@
+// sorgente img
+const img = [
+    './img/01.jpg',
+    './img/02.jpg',
+    './img/03.jpg',
+    './img/04.jpg',
+    './img/05.jpg'
+];
+
+//Acquisizione slider e arrows
+const sliderElment = document.querySelector('.slider');
+const arrowLeftElement = document.querySelector('.arrow_left');
+const arrowRightElement = document.querySelector('.arrow_right');
+
+// creazione elementi: DIV e IMG e Riempimento sorgenti img
+for (let i = 0; i < img.length; i++){
+    const slideElement = document.createElement('div');
+    slideElement.className = 'slide_wrapper';
+    // alla prima img active di default 
+    if (i === 0){
+        slideElement.classList.add('active');
+    }
+    
+    const slideImgElement = document.createElement('img');
+    slideImgElement.src = img[i];
+    
+    // aggiungiamo le img al div
+    slideElement.append(slideImgElement);
+    
+    // aggiungiamo le slide allo slider
+    sliderElment.append(slideElement);
+}
